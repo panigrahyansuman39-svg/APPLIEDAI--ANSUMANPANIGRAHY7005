22789a117c0bf38e96df723b43ff1530176d028b
panigrahyansuman39-svg <panigrahyansuman39@gmail.com>
2026-01-05 16:20:00 +0530

    string program

---
+++ python2.1.py
@@ -0,0 +1,9 @@
+#string wrinting in python
+str1="ansuman panigrahy"
+str2='ansuman panigrahy'
+str3="""ansuman panigrahy"""
+#why we use different types of quotes
+str4="anusman's book"#using a specific quote inside the string
+#escape sequence
+str1="ansuman \n panigrahy"#go to new line after \n
+str1="ansuman \t panigrahy"#tab space after \t
