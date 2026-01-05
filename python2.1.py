222c9a9edc74e6cce02b1e02ab08aafcad700ed8
panigrahyansuman39-svg <panigrahyansuman39@gmail.com>
2026-01-05 18:04:18 +0530

    string operation

--- python2.1.py
+++ python2.1.py
@@ -7,3 +7,7 @@ str4="anusman's book"#using a specific quote inside the string
 #escape sequence
 str1="ansuman \n panigrahy"#go to new line after \n
 str1="ansuman \t panigrahy"#tab space after \t
+print(str1)
+#string operation
+print(str1+str4)#string concatenation
+print(len(str4))#length of a string
